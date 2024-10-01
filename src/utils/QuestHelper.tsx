9e58037a14dd04798a65a8ebdaeb99c70f80962f
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-01 19:02:25 -0400

    Added bson for mongoids

--- src/utils/QuestHelper.tsx
+++ src/utils/QuestHelper.tsx
@@ -1,5 +1,10 @@
 import { IQuest } from "../types/models/eft/common/tables/IQuest";
+import { ObjectId } from 'bson';
 
 export const CreateEmptyQuest = ():IQuest => {
-    return 4;
+    const quest: IQuest = {
+        _id: new ObjectId().id.toString(),
+    };
+
+    return quest;
 };
